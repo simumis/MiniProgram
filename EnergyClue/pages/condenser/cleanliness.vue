cfde65ab8c3c547c919e7e3e51b6798bf49050fe
simumis <simumis@hotmail.com>
2022-03-10 22:11:39 +0800

    button disabled bind to isReady in computed section

--- EnergyClue/pages/condenser/cleanliness.vue
+++ EnergyClue/pages/condenser/cleanliness.vue
@@ -16,7 +16,7 @@
 		</view>
 		<!-- 按钮 -->
 		<view class="calc">
-			<button type="primary" @click="onCalc" :disabled="(input_text[0].value=='' && input_text[4].value=='') || input_text[1].value=='' || input_text[2].value=='' || input_text[3].value=='' || input_text[5].value=='' || input_text[6].value=='' || input_text[7].value=='' || input_text[8].value=='' || input_text[9].value=='' || input_text[10].value=='' || input_text[11].value=='' || input_text[12].value==''">
+			<button type="primary" @click="onCalc" :disabled="!isReady">
 				开始计算
 			</button>
 			<button @click="onClear" style="background-color: red;">清除输入</button>
@@ -45,137 +45,153 @@ import * as condenser from './condenser.js'
 import {Water, setupPX} from '../../common/jif97.js'
 
 export default {
-  data () {
-    return {
-      input_text: [
-		  {name:'凝汽器热负荷', value:'1100', default:'1100', unit:'MW'},
-		  {name:'凝汽器压力', value:'5', default:'5', unit:'kPa'},
-		  {name:'冷却水入口温度', value:'20', default:'20', unit:'℃'},
-		  {name:'冷却水出口温度', value:'30', default:'30', unit:'℃'},
-		  {name:'冷却水流量', value:'25000', default:'25000', unit:'kg/s'}, //凝汽器热负荷可屏蔽冷却水流量
-		  {name:'冷却水比热', value:'4.18', default:'4.18', unit:'kJ/(kg·℃)'},
-		  {name:'冷却水密度', value:'1000', default:'1000', unit:'kg/m³'},
-		  {name:'凝汽器换热面积', value:'54000', default:'54000', unit:'㎡'},
-		  {name:'冷却管外径', value:'25', default:'25', unit:'mm'},
-		  {name:'冷却管壁厚', value:'0.5', default:'0.5', unit:'mm'},
-		  {name:'冷却管数量', value:'55000', default:'55000', unit:'根'},
-		  {name:'流程数', value:'2', default:'2', unit:''},
-		  {name:'设计清洁系数', value:'0.9', default:'0.9', unit:''}
-	  ],
-	  tube_materials: ['HSn70-1(锡黄铜)', 'HA177-2(铝黄铜)', 'BFe30-1-1(铁白铜)', 'BFe10-1-1(铁白铜)', '碳钢', 'TP304,TP316,TP317(不锈钢)', 'TA1,TA2(钛合金)'],
-	  material_index: -1,
-	  results: [
-		  {name:'凝汽器饱和温度', value:'', unit:'℃'},
-		  {name:'冷却水温升', value:'', unit:'℃'},
-		  {name:'凝汽器端差', value:'', unit:'℃'},
-		  {name:'凝汽器清洁系数', value:'', unit:''},
-		  {name:'凝汽器清洁度', value:'', unit:'%'}
-	  ],
-	  notes: [
-		  '1、本程序依据《凝汽器与真空系统运行维护导则》(DL/T 932-2019)所列方法编制。',
-		  '2、本程序根据标准公式计算凝汽器变工况特性，未对特定凝汽器进行校核，计算结果仅供参考。',
-		  '3、输入区域中“凝汽器热负荷”和“冷却水流量”输入其一即可，当两项都有输入数据时，以“凝汽器热负荷”数据为准进行计算。'
-	  ]
-    }
-  },
-  onLoad() {
-	  // 默认管材设为钛合金
-	  this.material_index = this.tube_materials.length - 1;
-  },
-  methods: {
-	 onMaterialSelect: function(e) {
-		 let idx = e.detail.value;
-		 this.material_index = idx;
-	 },
-	 onCalc: function (e) {
-		let ps = parseFloat(this.input_text[1].value) // 凝汽器压力
-		let tw1 = parseFloat(this.input_text[2].value); // 冷却水入口温度
-		let tw2 = parseFloat(this.input_text[3].value); // 冷却水出口温度
-		let cp = parseFloat(this.input_text[5].value); // 冷却水比热
-		let rho = parseFloat(this.input_text[6].value); // 冷却水密度
-		let A = parseFloat(this.input_text[7].value); // 凝汽器换热面积
-		let d = parseFloat(this.input_text[8].value); // 冷却管外径
-		let m = parseFloat(this.input_text[9].value); // 冷却管壁厚
-		let n = parseFloat(this.input_text[10].value); // 冷却管数量
-		let fn = parseFloat(this.input_text[11].value); // 流程数
-		let bc0 = parseFloat(this.input_text[12].value); // 设计清洁系数
-		// 凝汽器热负荷及冷却水流量计算
-		let Q, Gw;
-		if(this.input_text[0].value != '') {
-			Q = parseFloat(this.input_text[0].value); // 凝汽器热负荷以输入数据为准
-			Gw = Q * 1000 / cp / (tw2 - tw1);
-		} else {
-			Gw = parseFloat(this.input_text[4].value); // 冷却水流量
-			Q = Gw * cp * (tw2 - tw1) / 1000;
+	data () {
+		return {
+			input_text: [
+				{name:'凝汽器热负荷', value:'1100', default:'1100', unit:'MW'},
+				{name:'凝汽器压力', value:'5', default:'5', unit:'kPa'},
+				{name:'冷却水入口温度', value:'20', default:'20', unit:'℃'},
+				{name:'冷却水出口温度', value:'30', default:'30', unit:'℃'},
+				{name:'冷却水流量', value:'25000', default:'25000', unit:'kg/s'}, //凝汽器热负荷可屏蔽冷却水流量
+				{name:'冷却水比热', value:'4.18', default:'4.18', unit:'kJ/(kg·℃)'},
+				{name:'冷却水密度', value:'1000', default:'1000', unit:'kg/m³'},
+				{name:'凝汽器换热面积', value:'54000', default:'54000', unit:'㎡'},
+				{name:'冷却管外径', value:'25', default:'25', unit:'mm'},
+				{name:'冷却管壁厚', value:'0.5', default:'0.5', unit:'mm'},
+				{name:'冷却管数量', value:'55000', default:'55000', unit:'根'},
+				{name:'流程数', value:'2', default:'2', unit:''},
+				{name:'设计清洁系数', value:'0.9', default:'0.9', unit:''}
+			],
+			tube_materials: ['HSn70-1(锡黄铜)', 'HA177-2(铝黄铜)', 'BFe30-1-1(铁白铜)', 'BFe10-1-1(铁白铜)', '碳钢', 'TP304,TP316,TP317(不锈钢)', 'TA1,TA2(钛合金)'],
+			material_index: -1,
+			results: [
+				{name:'凝汽器饱和温度', value:'', unit:'℃'},
+				{name:'冷却水温升', value:'', unit:'℃'},
+				{name:'凝汽器端差', value:'', unit:'℃'},
+				{name:'凝汽器清洁系数', value:'', unit:''},
+				{name:'凝汽器清洁度', value:'', unit:'%'}
+			],
+			notes: [
+				'1、本程序依据《凝汽器与真空系统运行维护导则》(DL/T 932-2019)所列方法编制。',
+				'2、本程序根据标准公式计算凝汽器变工况特性，未对特定凝汽器进行校核，计算结果仅供参考。',
+				'3、输入区域中“凝汽器热负荷”和“冷却水流量”输入其一即可，当两项都有输入数据时，以“凝汽器热负荷”数据为准进行计算。'
+			]
 		}
-		// 
-		let v = condenser.velocity(Gw, d, m, n, fn, rho);
-		let K0;
-		try{
-			K0 = condenser.K0(d, v);
-		}catch(e){
-			//TODO handle the exception
-			uni.showModal({
-				title: '警告',
-				content: e.message,
-				showCancel: false
-			});
-			// 清除输出结果
-			for(let item of this.results) {
-				item.value = '';
-			}
-			return;
-		}
-		// 计算凝汽器饱和温度
-		let water = setupPX(ps/1000, 1);
-		if(water == null) {
-			// 清除输出结果
-			for(let item of this.results) {
-				item.value = '';
-			}
-			uni.showModal({
-				title: '警告',
-				content: '请检查输入数据是否在有效范围。',
-				showCancel: false
-			});
-			return;
-		}
-		let ts = water.t - 273.15; 
-		let bt = condenser.bt(tw1);
-		let bm = condenser.bm(this.material_index, m);
-		let bc = condenser.cleanliness(Q, tw1, tw2, ts, A, K0, bt, bm);
-		let dt = tw2 - tw1; // 冷却水温升
-		let delta = ts - tw2; // 凝汽器端差
-		let cl = bc / bc0 * 100; // 凝汽器清洁度
-		
-		// 输出结果
-		this.results[0].value = ts.toPrecision(4);
-		this.results[1].value = dt.toPrecision(4);
-		this.results[2].value = delta.toPrecision(4);
-		this.results[3].value = bc.toPrecision(4);
-		this.results[4].value = cl.toPrecision(4);
-	 },
-	 onReset: function(e) {
-		for(let item of this.input_text) {
-			 item.value = item.default;
-		 }
+	},
+	onLoad() {
 		// 默认管材设为钛合金
-		 this.material_index = this.tube_materials.length - 1;
-		// 清除输出结果
-		for(let item of this.results) {
-			item.value = '';
+		this.material_index = this.tube_materials.length - 1;
+	},
+	computed: {
+		isReady: function() {
+			if(this.input_text[0].value=='' && this.input_text[4].value=='') {
+				return false;
+			}
+			for(let i=1; i<this.input_text.length; i++) {
+				if(i == 4) {
+					continue;
+				}
+				if(this.input_text[i].value == '') {
+					return false;
+				}
+			}
+			return true;
 		}
-	 },
-	 onClear: function(e) {
-		for(let item of this.input_text) {
-			item.value = '';
+	},
+	methods: {
+		onMaterialSelect: function(e) {
+			let idx = e.detail.value;
+			this.material_index = idx;
+		},
+		onCalc: function (e) {
+			let ps = parseFloat(this.input_text[1].value) // 凝汽器压力
+			let tw1 = parseFloat(this.input_text[2].value); // 冷却水入口温度
+			let tw2 = parseFloat(this.input_text[3].value); // 冷却水出口温度
+			let cp = parseFloat(this.input_text[5].value); // 冷却水比热
+			let rho = parseFloat(this.input_text[6].value); // 冷却水密度
+			let A = parseFloat(this.input_text[7].value); // 凝汽器换热面积
+			let d = parseFloat(this.input_text[8].value); // 冷却管外径
+			let m = parseFloat(this.input_text[9].value); // 冷却管壁厚
+			let n = parseFloat(this.input_text[10].value); // 冷却管数量
+			let fn = parseFloat(this.input_text[11].value); // 流程数
+			let bc0 = parseFloat(this.input_text[12].value); // 设计清洁系数
+			// 凝汽器热负荷及冷却水流量计算
+			let Q, Gw;
+			if(this.input_text[0].value != '') {
+				Q = parseFloat(this.input_text[0].value); // 凝汽器热负荷以输入数据为准
+				Gw = Q * 1000 / cp / (tw2 - tw1);
+			} else {
+				Gw = parseFloat(this.input_text[4].value); // 冷却水流量
+				Q = Gw * cp * (tw2 - tw1) / 1000;
+			}
+			// 
+			let v = condenser.velocity(Gw, d, m, n, fn, rho);
+			let K0;
+			try{
+				K0 = condenser.K0(d, v);
+			}catch(e){
+				//TODO handle the exception
+				uni.showModal({
+					title: '警告',
+					content: e.message,
+					showCancel: false
+				});
+				// 清除输出结果
+				for(let item of this.results) {
+					item.value = '';
+				}
+				return;
+			}
+			// 计算凝汽器饱和温度
+			let water = setupPX(ps/1000, 1);
+			if(water == null) {
+				// 清除输出结果
+				for(let item of this.results) {
+					item.value = '';
+				}
+				uni.showModal({
+					title: '警告',
+					content: '请检查输入数据是否在有效范围。',
+					showCancel: false
+				});
+				return;
+			}
+			let ts = water.t - 273.15; 
+			let bt = condenser.bt(tw1);
+			let bm = condenser.bm(this.material_index, m);
+			let bc = condenser.cleanliness(Q, tw1, tw2, ts, A, K0, bt, bm);
+			let dt = tw2 - tw1; // 冷却水温升
+			let delta = ts - tw2; // 凝汽器端差
+			let cl = bc / bc0 * 100; // 凝汽器清洁度
+			
+			// 输出结果
+			this.results[0].value = ts.toPrecision(4);
+			this.results[1].value = dt.toPrecision(4);
+			this.results[2].value = delta.toPrecision(4);
+			this.results[3].value = bc.toPrecision(4);
+			this.results[4].value = cl.toPrecision(4);
+		},
+		onReset: function(e) {
+			for(let item of this.input_text) {
+				 item.value = item.default;
+			}
+			// 默认管材设为钛合金
+			 this.material_index = this.tube_materials.length - 1;
+			// 清除输出结果
+			for(let item of this.results) {
+				item.value = '';
+			}
+		},
+		onClear: function(e) {
+			for(let item of this.input_text) {
+				item.value = '';
+			}
+			// 清除输出结果
+			for(let item of this.results) {
+				item.value = '';
+			}
 		}
-		// 清除输出结果
-		for(let item of this.results) {
-			item.value = '';
-		}
-	 }
-  }
+	}
 }
 </script>
 
