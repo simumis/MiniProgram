021f237e45b765aad351ab3aad34258d68c68082
simumis <simumis@hotmail.com>
2022-03-12 15:36:08 +0800

    update index

--- EnergyClue/pages/index/index.vue
+++ EnergyClue/pages/index/index.vue
@@ -1,14 +1,12 @@
 <template>
 	<!-- 首页放置导航按钮，每行并排放两个 -->
 	<view class="content">
-		<view class="utils-panel" v-for="(item, idx) in utils" :key=idx>
-			<view class="utils-item" hover-class="utils-item-hover" @click="goto(item.left.page)">
-				<image class="utils-item-img" :src="item.left.image"></image>
-				<text class="utils-item-text">{{item.left.name}}</text>
-			</view>
-			<view class="utils-item" hover-class="utils-item-hover" @click="goto(item.right.page)">
-				<image class="utils-item-img" :src="item.right.image"></image>
-				<text class="utils-item-text">{{item.right.name}}</text>
+		<view class="utils-area">
+			<view class="utils-panel" v-for="(item, idx) in utils" :key="idx">
+				<view class="utils-item" hover-class="utils-item-hover" v-for="(util,i) in item" :key="i" @click="goto(util.page)">
+					<image class="utils-item-img" :src="util.image"></image>
+					<text class="utils-item-text">{{util.name}}</text>
+				</view>
 			</view>
 		</view>
 	</view>
@@ -59,11 +57,25 @@
 </script>
 
 <style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: flex-start;
+	}
+	
+	.utils-area {
+		display: flex;
+		flex-direction: column;
+		align-items: stretch;
+		justify-content: center;
+	}
+	
 	.utils-panel {
 		display: flex;
 		flex-direction: row;
-		align-items: stretch;
-		justify-content: center;
+		align-items: flex;
+		justify-content: flex-start;
 	}
 	
 	.utils-item, .utils-item-hover {
